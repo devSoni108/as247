bf9427a524504f0046db07b2c2b824e55311dbbe
devSoni <soni.hiren108@gmail.com>
2023-07-25 17:35:43 +0200

    revert changes

--- app/page.js
+++ app/page.js
@@ -1,23 +1,9 @@
-import Hero from "@/components/Hero"
-import Protection from "@/components/Protection"
-import Brands from "@/components/Brands"
-import Services from "@/components/Services"
-import Testimonials from "@/components/Testimonials"
-import FAQ from "@/components/FAQ"
-import CTA from "@/components/CTA"
-import Footer from "@/components/Footer"
+import ComingSoon from "@/components/Soon"
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <Protection />
-      <Brands />
-      <Services />
-      <Testimonials />
-      <FAQ />
-      <CTA />
-      <Footer />
+      <ComingSoon />
     </main>
   )
 }
---
+++ app/pages/home.js
@@ -0,0 +1,25 @@
+import Hero from "@/components/Hero"
+import Protection from "@/components/Protection"
+import Brands from "@/components/Brands"
+import Services from "@/components/Services"
+import Testimonials from "@/components/Testimonials"
+import FAQ from "@/components/FAQ"
+import CTA from "@/components/CTA"
+import Footer from "@/components/Footer"
+
+const Welcome = () => {
+    return (
+        <main>
+            <Hero />
+            <Protection />
+            <Brands />
+            <Services />
+            <Testimonials />
+            <FAQ />
+            <CTA />
+            <Footer />
+        </main>
+    )
+}
+
+export default Welcome
