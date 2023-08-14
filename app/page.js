d436abd3a1a642c8b449a3f2609a99598c1fec8e
devSoni <soni.hiren108@gmail.com>
2023-08-14 12:06:43 +0200

    ComingSoon

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Hero from "@/components/Hero"
+import ComingSoon from "@/components/Soon"
 
 export default function Home() {
   return (
     <main>
-      <Hero />      
+      <ComingSoon />   
     </main>
   )
 }
